85a9aa58860763b3f386c8489a212b303d7877b2
uriasvitor <uriasvitor@gmail.com>
2022-12-17 14:01:58 -0300

    tic finished

--- tic-tac-toe/scripts/index.js
+++ tic-tac-toe/scripts/index.js
@@ -1,53 +1,89 @@
-(()=>{
-    const handleSquare = document.querySelectorAll("td")
-    var btn = document.getElementById("btn")
-    var Player      = []
-    var playerOne   = []
-    var playerTwo   = []
-    
-    btn.addEventListener('click', ()=>{
-        startGame()
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
+
+startGame()
+
+restartButton.addEventListener('click', startGame)
+
+function startGame() {
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
+}
+
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
+}
+
+function endGame(draw) {
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+  }
+  winningMessageElement.classList.add('show')
+}
+
+function isDraw() {
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(X_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(X_CLASS)
+  }
+}
+
+function checkWin(currentClass) {
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
     })
-
-
-    function startGame(){
-        createGame()
-    }
-
-    function createGame(){
-        randomSimbol()
-
-        for(let i = 0; i < handleSquare.length; i++){
-            handleSquare[i].addEventListener("click",()=>{
-                handleSquare[i].innerHTML = player
-            })
-        }
-    }
-
-    function randomSimbol(){
-        var simbols     = ['X', '0']
-        const random = Math.floor(Math.random()* simbols.length)
-        playerOne = random
-        
-        if(playerOne == 0){
-            playerTwo = 1
-        }else{
-            playerOne = 1
-            playerTwo = 0
-        }
-
-        roundMatch(playerOne, playerTwo)
-
-    }
-
-    function roundMatch(playerOne,playerTwo){
-        players = [playerOne,playerTwo]
-        player = players.length -1
-
-        console.log(players)
-    }
-
-
-
-
-})();
+  })
+}
